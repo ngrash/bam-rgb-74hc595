3df58474b0f05ae03adfde739a0a1952eb44a0b1
Nico Grashoff <ngrash@gmail.com>
2017-02-06 10:19:31 +0100

    Add late-night improvements
    * Use timer
    * Fix off-by-one error
    * Implement static color wheel

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,3 +1,9 @@
+/*
+   TODO
+   - use 8-bit timer
+   - use volatile variables
+   - double buffer
+*/
 #define SI_PIN 12
 #define SCK_PIN 13
 #define RCK_PIN 11
@@ -8,21 +14,30 @@
 
 #define BAM_POSITIONS 4
 #define OUT_BYTES 3
-
-/*
-   TODO
-   - use timer interrupt
-*/
+#define TIMER_COUNT 60000
 
 long _lastBrightnessChange;
 byte _brightness[NUM_LEDS];
+byte _bamCounter;
+byte _bamBytes[BAM_POSITIONS][OUT_BYTES];
 
-void setLed(byte index, byte r, byte g, byte b) {
-  int ledPos = index * 3;
-  _brightness[ledPos + 0] = r;
-  _brightness[ledPos + 1] = g;
-  _brightness[ledPos + 2] = b;
-}
+byte _wheelPosition[8];
+
+#define WHEEL_COLORS 8
+byte _colors[][3] = {
+  {255,   0,   0},
+  {255, 127,   0},
+  {255, 255,   0},
+  {127, 255,   0},
+  {  0, 255,   0},
+  {  0, 255, 127},
+  {  0, 255, 255},
+  {  0, 127, 255},
+  {  0,   0, 255},
+  {127,   0, 255},
+  {255,   0, 255},
+  {255,   0, 127}
+};
 
 void setup()
 {
@@ -30,80 +45,65 @@ void setup()
   pinMode(SCK_PIN, OUTPUT);
   pinMode(RCK_PIN, OUTPUT);
   digitalWrite(RCK_PIN, LOW);
+
+  noInterrupts();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = TIMER_COUNT;
+  TCCR1B |= (1 << CS10);
+  TIMSK1 |= (1 << TOIE1);
+  interrupts();
+
+  for(int i = 0; i < 8; i++) {
+    _wheelPosition[i] = i;
+  }
+}
+
+ISR(TIMER1_OVF_vect)
+{
+  TCNT1 = TIMER_COUNT;
+  bam();
 }
 
 void loop()
 {
   long currentMillis = millis();
-  if(currentMillis - _lastBrightnessChange >= 1000) {
+  if(currentMillis - _lastBrightnessChange >= 50) {
     _lastBrightnessChange = currentMillis;
 
-    _brightness[2]++;
-    if(_brightness[2] > BAM_MAX_VALUE) _brightness[2] = 0;
+    for(int i = 0; i < 8; i++) {
+      _wheelPosition[i]++;
+      if(_wheelPosition[i] > WHEEL_COLORS) _wheelPosition[i] = 0;
+
+      byte pos = i*3;
+      byte *color = _colors[_wheelPosition[i]];
+      _brightness[pos+0] = color[0];
+      _brightness[pos+1] = color[1];
+      _brightness[pos+2] = color[2];
+    }
 
     bamCalc(_brightness, NUM_LEDS);
   }
-
-  bam();
 }
-
-void wheel(byte *r, byte *g, byte *b) {
-  if(*r == 0 && *g == 0 && *b == 0) *r = WHEEL_MAX_BRIGHTNESS;
-
-  if(*r == WHEEL_MAX_BRIGHTNESS && *b == 0) {
-    if(*g == WHEEL_MAX_BRIGHTNESS) --*r;
-    else ++*g;
-  }
-  else if(*g == WHEEL_MAX_BRIGHTNESS && *b == 0) {
-    if(*r == 0) ++*b;
-    else --*r;
-  }
-  else if(*g == WHEEL_MAX_BRIGHTNESS && *r == 0) {
-    if(*b == WHEEL_MAX_BRIGHTNESS) --*g;
-    else ++*b;
-  }
-  else if(*b == WHEEL_MAX_BRIGHTNESS && *r == 0) {
-    if(*g == 0) ++*r;
-    else --*g;
-  }
-  else if(*b == WHEEL_MAX_BRIGHTNESS && *g == 0) {
-    if(*r == WHEEL_MAX_BRIGHTNESS) --*b;
-    else ++*r;
-  }
-  else if(*r == WHEEL_MAX_BRIGHTNESS && *g == 0) {
-    if(*b == 0) ++*g;
-    else --*b;
-  }
-}
-
-byte _bamCounter;
-unsigned long _lastBamWrite;
 
 void bam()
 {
-  unsigned long currentMicros = micros();
-  if(currentMicros - _lastBamWrite >= 50) {
-    _lastBamWrite = currentMicros;
+  _bamCounter++;
+  if(_bamCounter > BAM_MAX_VALUE) _bamCounter = B00000001;
 
-    _bamCounter++;
-    if(_bamCounter > BAM_MAX_VALUE) _bamCounter = B00000001;
-
-    if(_bamCounter == B00000001) bamWrite(1);
-    if(_bamCounter == B00000010) bamWrite(2);
-    if(_bamCounter == B00000100) bamWrite(3);
-    if(_bamCounter == B00001000) bamWrite(4);
-    if(_bamCounter == B00010000) bamWrite(5);
-    if(_bamCounter == B00100000) bamWrite(6);
-    if(_bamCounter == B01000000) bamWrite(7);
-    if(_bamCounter == B10000000) bamWrite(8);
-  }
+  if(_bamCounter == B00000001) bamWrite(0);
+  if(_bamCounter == B00000010) bamWrite(1);
+  if(_bamCounter == B00000100) bamWrite(2);
+  if(_bamCounter == B00001000) bamWrite(3);
+  if(_bamCounter == B00010000) bamWrite(4);
+  if(_bamCounter == B00100000) bamWrite(5);
+  if(_bamCounter == B01000000) bamWrite(6);
+  if(_bamCounter == B10000000) bamWrite(7);
 }
 
-byte _bamBytes[BAM_POSITIONS][OUT_BYTES];
-
 void bamCalc(byte leds[], byte ledsSize) {
-  for(int pos = 1; pos <= BAM_POSITIONS; pos++) {
-    bamBytes(pos, leds, ledsSize, _bamBytes[pos-1]);
+  for(int pos = 0; pos < BAM_POSITIONS; pos++) {
+    bamBytes(pos, leds, ledsSize, _bamBytes[pos]);
   }
 }
 
@@ -131,7 +131,7 @@ void bamBytes(byte pos, byte leds[], byte ledsSize, byte *buffer) {
 
 void bamWrite(int pos) {
   for(int i = 0; i < OUT_BYTES; i++) {
-    shiftOut(SI_PIN, SCK_PIN, LSBFIRST, _bamBytes[pos-1][i]);
+    shiftOut(SI_PIN, SCK_PIN, LSBFIRST, _bamBytes[pos][i]);
   }
   digitalWrite(RCK_PIN, HIGH);
   digitalWrite(RCK_PIN, LOW);
